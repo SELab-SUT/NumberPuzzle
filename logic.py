8de6dd315b7ffbd74e841a9b791a682ef900b832
Kiarash Golzadeh <kiarash.gl@gmail.com>
2021-03-11 15:49:22 +0330

    Implement first version of Game class

--- logic.py
+++ logic.py
@@ -1,12 +1,21 @@
 class Game:
-	def __init__(self, position):
-		self.position = position.copy()
+	def __init__(self):
+		self.position = []
+		self.__empty_position = self.__find_empty_place();
 
-	def move(self, move_type):
+	def move(self, move_position):
 		raise NotImplementedError
 
-	def __check_validity(self, move_type):
+	def __check_validity(self, move_position):
 		raise NotImplementedError
 
-	def __check_finished(self):
+	def __find_empty_place(self):
+		for i in range(len(self.position)):
+			for j in range(len(self.position[i])):
+				if self.position[i][j] == '*':
+					return (i, j)
+
+		raise Exception("There is no empty place in the board.")
+
+	def has_finished(self):
 		raise NotImplementedError
