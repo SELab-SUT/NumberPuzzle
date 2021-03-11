30383abe9de9426f64db97986def1bd7cffa873d
Kiarash Golzadeh <kiarash.gl@gmail.com>
2021-03-11 16:04:16 +0330

    Implement run function based on the prototypes of Game and view

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from logic import Game
+from view import *
+
 def run():
-	raise NotImplementedError
+	game = Game()
+	print_commands()
+	while not game.has_finished():
+		print_game(game)
+		input_xy = take_input()
+		game.move(input_xy)
+	print("Game over, You won!")
 
 
 if __name__=='__main__':
