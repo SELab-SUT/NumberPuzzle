c85977a39303ab0e46d345eebaa3710d26cb5275
Alireza Mousavi <moosavi78@gmail.com>
2021-03-11 16:44:10 +0330

    Implement an early version of run

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from logic import Game
+from view import *
+
+
 def run():
-	raise NotImplementedError
+	game = Game([[1, 2, 3], [4, 5, 6], [7, '*', 8]])
+	while True:
+		print_game(game)
+		print_commands()
+		game.move(take_input())
 
 
 if __name__=='__main__':
