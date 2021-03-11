6943494b808bf5bf2e4d9c8f755aca0718d2193e
Kiarash Golzadeh <kiarash.gl@gmail.com>
2021-03-11 16:10:07 +0330

    Initialize board using a generated sample

--- logic.py
+++ logic.py
@@ -1,6 +1,10 @@
 class Game:
 	def __init__(self):
-		self.position = []
+		self.__width = 3
+		self.__height = 3
+		self.position = [[8, 3, 4],
+						 [2, 1, 7],
+						 [5, 6, '*']]
 		self.__empty_position = self.__find_empty_place();
 
 	def move(self, move_position):
@@ -10,12 +14,13 @@ class Game:
 		raise NotImplementedError
 
 	def __find_empty_place(self):
-		for i in range(len(self.position)):
-			for j in range(len(self.position[i])):
+		for i in range(self.__height):
+			for j in range(self.__width):
 				if self.position[i][j] == '*':
 					return (i, j)
 
 		raise Exception("There is no empty place in the board.")
 
 	def has_finished(self):
-		raise NotImplementedError
+		# TODO: Return state of game based on the board
+		return False
