ae02b44c567b2cfe88fafa0c4c90730d39830778
Kiarash Golzadeh <kiarash.gl@gmail.com>
2021-03-11 16:52:32 +0330

    Implement moving tiles

--- logic.py
+++ logic.py
@@ -1,21 +1,31 @@
 class Game:
 	def __init__(self):
-		self.__width = 3
-		self.__height = 3
+		self.__cols = 3
+		self.__rows = 3
 		self.position = [[8, 3, 4],
 						 [2, 1, 7],
 						 [5, 6, '*']]
 		self.__empty_position = self.__find_empty_place();
 
-	def move(self, move_position):
-		raise NotImplementedError
+	def move(self, move_type):
+		move_position = self.__get_moving_tile(move_type)
+		self.__check_validity(move_position)
+		empty_x, empty_y = self.__empty_position
+		move_x, move_y = move_position
+		self.position[move_x][move_y], self.position[empty_x][empty_y] = self.position[empty_x][empty_y], self.position[move_x][move_y]
+		self.__empty_position = move_position
+
+	def __get_moving_tile(self, move_type):
+		next_tile_distance = {'L': (0, 1), 'U': (1, 0), 'R': (0, -1), 'D': (-1, 0)}
+		return (self.__empty_position[0] + next_tile_distance[move_type][0], self.__empty_position[1] + next_tile_distance[move_type][1])
 
 	def __check_validity(self, move_position):
-		raise NotImplementedError
+		if move_position[0] < 0 or move_position[1] < 0 or move_position[0] >= self.__rows or move_position[1] >= self.__cols:
+			raise Exception("Moving tile position will be outside of the board.")
 
 	def __find_empty_place(self):
-		for i in range(self.__height):
-			for j in range(self.__width):
+		for i in range(self.__rows):
+			for j in range(self.__cols):
 				if self.position[i][j] == '*':
 					return (i, j)
 
